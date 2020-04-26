57ed56c84a404f0beb6b8df19c4209446f9dfc9d
Gabriel Thiago de Oliveira <gabrieum99@gmail.com>
2020-04-26 02:25:01 -0300

    configurando database

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ require('dotenv/config');
 
 module.exports = {
     dialect: 'postgres',
-    host: process.env.DATABASE_URL,
+    host: process.env.DB_HOST,
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
